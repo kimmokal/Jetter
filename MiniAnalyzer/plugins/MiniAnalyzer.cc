434afd5ccd7c65117f6f41d2ec2ce15a5b10f98f
Petra Ekroos <petra.ekroos@aalto.fi>
2017-08-08 16:48:38 +0200

    First test tree production

--- MiniAnalyzer/plugins/MiniAnalyzer.cc
+++ MiniAnalyzer/plugins/MiniAnalyzer.cc
@@ -31,6 +31,12 @@
 #include "DataFormats/PatCandidates/interface/MET.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
 
+#include "TTree.h"
+#include "TFile.h"
+//#include "Jetter/MiniAnalyzer/plugins/MiniAnalyzer.h"
+
+
+
 //
 // class declaration
 //
@@ -41,38 +47,61 @@ class MiniAnalyzer : public edm::EDAnalyzer {
       ~MiniAnalyzer();
 
    private:
+//      virtual void beginJob() override;
       virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+//      virtual void endJob() override;
 
       // ----------member data ---------------------------
       edm::EDGetTokenT<reco::VertexCollection> vtxToken_;
       edm::EDGetTokenT<pat::JetCollection> jetToken_;
+      
+      TFile* outputFile;
+      TTree* tree;
+      float pT;
+      
+
 };
 
 MiniAnalyzer::MiniAnalyzer(const edm::ParameterSet& iConfig):
     vtxToken_(consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("vertices"))),
     jetToken_(consumes<pat::JetCollection>(iConfig.getParameter<edm::InputTag>("jets")))
 {
+
+//tanne branch maarittelyt
+	outputFile = new TFile("jetted_tree.root","recreate");
+	tree = new TTree("jet_tree", "A simplified jet tree");
+
+	tree->Branch("pT", &pT, "pT/F");
+	
 }
 
 MiniAnalyzer::~MiniAnalyzer()
 {
+
+//lopettaessa
+	tree->Write();
+	outputFile->Close();
+
+
 }
 
 
 void
 MiniAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-    edm::Handle<reco::VertexCollection> vertices;
+/*    edm::Handle<reco::VertexCollection> vertices;
     iEvent.getByToken(vtxToken_, vertices);
     if (vertices->empty()) return; // skip the event if no PV found
     const reco::Vertex &PV = vertices->front();
-
+*/
     edm::Handle<pat::JetCollection> jets;
     iEvent.getByToken(jetToken_, jets);
-    int ijet = 0;
+//    int ijet = 0;
     for (const pat::Jet &j : *jets) {
         if (j.pt() < 20) continue;
-        printf("jet  with pt %5.1f (raw pt %5.1f), eta %+4.2f, btag CSV %.3f, CISV %.3f, pileup mva disc %+.2f\n",
+		pT = j.pt();
+
+/*        printf("jet  with pt %5.1f (raw pt %5.1f), eta %+4.2f, btag CSV %.3f, CISV %.3f, pileup mva disc %+.2f\n",
             j.pt(), j.pt()*j.jecFactor("Uncorrected"), j.eta(), std::max(0.f,j.bDiscriminator("combinedSecondaryVertexBJetTags")), std::max(0.f,j.bDiscriminator("combinedInclusiveSecondaryVertexBJetTags")), j.userFloat("pileupJetId:fullDiscriminant"));
         if ((++ijet) == 1) { // for the first jet, let's print the leading constituents
             std::vector<reco::CandidatePtr> daus(j.daughterPtrVector());
@@ -80,11 +109,16 @@ MiniAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
             for (unsigned int i2 = 0, n = daus.size(); i2 < n && i2 <= 3; ++i2) {
                 const pat::PackedCandidate &cand = dynamic_cast<const pat::PackedCandidate &>(*daus[i2]);
                 printf("         constituent %3d: pt %6.2f, dz(pv) %+.3f, pdgId %+3d\n", i2,cand.pt(),cand.dz(PV.position()),cand.pdgId());
+
             }
         }
+*/
+
+	tree->Fill();
+
     }
 
-    printf("\n");
+//    printf("\n");
 }
 
 //define this as a plug-in
