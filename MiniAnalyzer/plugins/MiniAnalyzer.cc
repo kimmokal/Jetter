4392b4b2f5e320eaca3585b60c9308592b641dd8
Petra <petra.ekroos@aalto.fi>
2017-08-09 14:22:50 +0200

    added event data branches

--- MiniAnalyzer/plugins/MiniAnalyzer.cc
+++ MiniAnalyzer/plugins/MiniAnalyzer.cc
@@ -35,32 +35,36 @@
 #include "TFile.h"
 //#include "Jetter/MiniAnalyzer/plugins/MiniAnalyzer.h"
 
-
-
 //
 // class declaration
 //
 
 class MiniAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit MiniAnalyzer(const edm::ParameterSet&);
-      ~MiniAnalyzer();
+    public:
+        explicit MiniAnalyzer(const edm::ParameterSet&);
+        ~MiniAnalyzer();
 
-   private:
-//      virtual void beginJob() override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-//      virtual void endJob() override;
+    private:
+        virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
 
-      // ----------member data ---------------------------
-      edm::EDGetTokenT<reco::VertexCollection> vtxToken_;
-      edm::EDGetTokenT<pat::JetCollection> jetToken_;
-      
-      TFile* outputFile;
-      TTree* tree;
-      float pT;
-      
+        // ----------member data ---------------------------
+        edm::EDGetTokenT<reco::VertexCollection> vtxToken_;
+        edm::EDGetTokenT<pat::JetCollection> jetToken_;
+        
+        TFile* outputFile;
+        TTree* tree;
+
+        float pT;
+        float eta;
+        float phi;
+        float E;
+
+        unsigned int event;
+        unsigned int run;
+        unsigned int lumi;
 
 };
+
 
 MiniAnalyzer::MiniAnalyzer(const edm::ParameterSet& iConfig):
     vtxToken_(consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("vertices"))),
@@ -68,11 +72,18 @@ MiniAnalyzer::MiniAnalyzer(const edm::ParameterSet& iConfig):
 {
 
 //tanne branch maarittelyt
-	outputFile = new TFile("jetted_tree.root","recreate");
-	tree = new TTree("jet_tree", "A simplified jet tree");
+    outputFile = new TFile("nanotuple.root","recreate");
+    tree = new TTree("jet_tree", "A simplified jet tree");
 
-	tree->Branch("pT", &pT, "pT/F");
+    tree->Branch("pT", &pT, "pT/F");
+    tree->Branch("eta", &eta, "eta/F");
+    tree->Branch("phi", &phi, "phi/F");
+    tree->Branch("E", &E, "E/F");
 	
+    tree->Branch("event", &event, "event/l");
+    tree->Branch("run", &run, "run/l");
+    tree->Branch("lumi", &lumi, "lumi/l");
+
 }
 
 MiniAnalyzer::~MiniAnalyzer()
@@ -82,43 +93,20 @@ MiniAnalyzer::~MiniAnalyzer()
 	tree->Write();
 	outputFile->Close();
 
-
 }
 
 
 void
 MiniAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-/*    edm::Handle<reco::VertexCollection> vertices;
-    iEvent.getByToken(vtxToken_, vertices);
-    if (vertices->empty()) return; // skip the event if no PV found
-    const reco::Vertex &PV = vertices->front();
-*/
     edm::Handle<pat::JetCollection> jets;
     iEvent.getByToken(jetToken_, jets);
-//    int ijet = 0;
     for (const pat::Jet &j : *jets) {
         if (j.pt() < 20) continue;
-		pT = j.pt();
+        pT = j.pt();
 
-/*        printf("jet  with pt %5.1f (raw pt %5.1f), eta %+4.2f, btag CSV %.3f, CISV %.3f, pileup mva disc %+.2f\n",
-            j.pt(), j.pt()*j.jecFactor("Uncorrected"), j.eta(), std::max(0.f,j.bDiscriminator("combinedSecondaryVertexBJetTags")), std::max(0.f,j.bDiscriminator("combinedInclusiveSecondaryVertexBJetTags")), j.userFloat("pileupJetId:fullDiscriminant"));
-        if ((++ijet) == 1) { // for the first jet, let's print the leading constituents
-            std::vector<reco::CandidatePtr> daus(j.daughterPtrVector());
-            std::sort(daus.begin(), daus.end(), [](const reco::CandidatePtr &p1, const reco::CandidatePtr &p2) { return p1->pt() > p2->pt(); }); // the joys of C++11
-            for (unsigned int i2 = 0, n = daus.size(); i2 < n && i2 <= 3; ++i2) {
-                const pat::PackedCandidate &cand = dynamic_cast<const pat::PackedCandidate &>(*daus[i2]);
-                printf("         constituent %3d: pt %6.2f, dz(pv) %+.3f, pdgId %+3d\n", i2,cand.pt(),cand.dz(PV.position()),cand.pdgId());
-
-            }
-        }
-*/
-
-	tree->Fill();
-
+        tree->Fill();
     }
-
-//    printf("\n");
 }
 
 //define this as a plug-in
