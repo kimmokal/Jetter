da2bc592a7f872545ad1c99ba9843ac47900708d
Mikko Voutilainen <mikko.voutilainen@cern.ch>
2017-09-21 11:45:09 +0300

    Modify config.

--- MiniAnalyzer/python/ConfFile_cfg.py
+++ MiniAnalyzer/python/ConfFile_cfg.py
@@ -26,7 +26,7 @@ process.demo = cms.EDAnalyzer('MiniAnalyzer',
     jets = cms.InputTag("slimmedJets"),
     mets = cms.InputTag("slimmedMETs"),
     pfCands = cms.InputTag("packedPFCandidates"),
-    genCands = cms.InputTag("packedGenParticles"),
+    genParticles = cms.InputTag("packedGenParticles"),
 )
 
 
