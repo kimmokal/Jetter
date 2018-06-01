248194896dac445e10ef26aba73cd5bbb62469f3
Kimmo Kallonen <kimmo.kallonen@cern.ch>
2018-06-01 14:11:35 +0200

    Add file sources

--- MiniAnalyzer/python/ConfFile_cfg.py
+++ MiniAnalyzer/python/ConfFile_cfg.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 import FWCore.PythonUtilities.LumiList as LumiList
+import FWCore.Utilities.FileUtils as FileUtils
 from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
 
 #process.source.lumisToProcess = LumiList.LumiList(filename = 'goodList.json').getVLuminosityBlockRange()
@@ -18,13 +19,17 @@ process.QGTagger.jetsLabel = cms.string("QGL_AK4PFchs")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 #Select the MiniAOD file to process
+# With pileup:
+# fileList = FileUtils.loadListFromFile('RunIISummer16MiniAODv2_QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8_PUMoriond17_magnetOn_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1_filelist.txt')
+# Without pileup:
+# fileList = FileUtils.loadListFromFile('RunIISummer16MiniAODv2_QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8_NoPU_magnetOn_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1_filelist.txt')
+
+#'/store/mc/RunIISummer16MiniAODv2/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/PUMoriond17_magnetOn_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/100000/08278E4E-E4EF-E611-8BD7-FA163E3ABA64.root'
+#'/store/mc/RunIISummer16MiniAODv2/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/NoPU_magnetOn_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/80000/DCD55D97-61F0-E611-9C1B-FA163EAD13C1.root'
+
 process.source = cms.Source("PoolSource",
-	fileNames = cms.untracked.vstring(
-	# Without pileup:
-	#'/store/mc/RunIISummer16MiniAODv2/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/NoPU_magnetOn_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/80000/DCD55D97-61F0-E611-9C1B-FA163EAD13C1.root'
-	# With pileup:
-	'/store/mc/RunIISummer16MiniAODv2/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/PUMoriond17_magnetOn_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/100000/08278E4E-E4EF-E611-8BD7-FA163E3ABA64.root'
-	)
+	# fileNames = cms.untracked.vstring(*fileList)
+	fileNames = cms.untracked.vstring('/store/mc/RunIISummer16MiniAODv2/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/PUMoriond17_magnetOn_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/100000/08278E4E-E4EF-E611-8BD7-FA163E3ABA64.root')
 )
 
 process.demo = cms.EDAnalyzer('MiniAnalyzer',
@@ -38,7 +43,8 @@ process.demo = cms.EDAnalyzer('MiniAnalyzer',
     mets = cms.InputTag("slimmedMETs"),
     pfCands = cms.InputTag("packedPFCandidates"),
     packed = cms.InputTag("packedGenParticles"),
-    genJets = cms.InputTag("slimmedGenJets")
+    genJets = cms.InputTag("slimmedGenJets"),
+	genEventInfo = cms.InputTag("generator")
 
 )
 
@@ -46,4 +52,3 @@ process.demo = cms.EDAnalyzer('MiniAnalyzer',
 process.p = cms.Path(process.QGTagger + process.demo)
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 10
-
